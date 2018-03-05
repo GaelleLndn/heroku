8b720639af19fef239e38403e94360e79e6aad9d
GaelleLndn <gaellelondon@hotmail.com>
2018-03-05 15:18:12 +0100

    init files

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 const port = process.env.PORT || 3000 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port 3000!'))

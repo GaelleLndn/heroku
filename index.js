ce67b739efd6aff48b7912510646d79137247840
Gaëlle <gaelle@MacBook-de-Gaelle.local>
2018-03-09 11:35:32 +0100

    heroku bdd

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+require ('dotenv').config();
+
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { makeExecutableSchema } = require('graphql-tools')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { Person } = require('./server/model')
+const schema = require('./server/schema')
+const resolvers = require('./server/resolvers');
+
+app.use(cors());
+
+mongoose.connect(
+    process.env.DATABASE_URL,
+        err =>{
+            if(err){
+                return console.log (err)
+            }
+        console.log("mongoDB ok")
+        }
+    );
+
+
+const execSchema = makeExecutableSchema({
+  typeDefs: schema,
+  resolvers
+});
+
+app.use('/graphql', express.json(), graphqlExpress({ schema: execSchema, context: { Person }}))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql'}))
 
 app.use ('', express.static(__dirname + '/herokulabapp/dist'))
 
